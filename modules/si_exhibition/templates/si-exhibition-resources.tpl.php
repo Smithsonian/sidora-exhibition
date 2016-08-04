ddbfa40be69497ef8eb87ff0cca4b390b9df3ae7
Robert Anderson <randerson@quotient-inc.com>
2016-08-04 17:12:24 -0400

    Put in information for debugging

--- modules/si_exhibition/templates/si-exhibition-resources.tpl.php
+++ modules/si_exhibition/templates/si-exhibition-resources.tpl.php
@@ -1,3 +1,11 @@
+<div class="hidden" style="display:none;">
+<div class="controls">
+   <?php print theme('item_list', array('items'=>$limit_types, 'title'=>t('Limit by type'), 'type'=>'ul', 'attributes'=>array('class' => 'limit-by-type'))) ?>
+</div>
+<div class="controls">
+   <?php print  theme('item_list', array('items'=>$results_per_page, 'title'=>t('Results per page'), 'type'=>'ul', 'attributes'=>array('class' => 'results-per-page'))); ?>
+</div>
+</div>
 <?php
 $toPrintForm = drupal_get_form('exhibition_resources_form');
 print render($toPrintForm);
